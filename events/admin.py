91f065eaebc357eea23e633b298964445164ff3d
MD Abu Sayed Khan <sayed196002@gmail.com>
2026-09-22 14:53:52 +0600

    Use Cloudinary field in event admin

--- events/admin.py
+++ events/admin.py
@@ -1,20 +1,32 @@
+from django import forms
 from django.contrib import admin
+from cloudinary.forms import CloudinaryFileField
+
 from .models import Event, Registration, EventRequest
 
 
-admin.site.register(Event)
+class EventAdminForm(forms.ModelForm):
+    image = CloudinaryFileField(required=False)
+
+    class Meta:
+        model = Event
+        fields = '__all__'
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    form = EventAdminForm
+
+
 admin.site.register(Registration)
 
 
 @admin.action(description='Create Event from approved request')
 def create_event_from_request(modeladmin, request, queryset):
-
     approved_requests = queryset.filter(status='Approved')
-
     created_count = 0
 
     for event_request in approved_requests:
-
         Event.objects.create(
             title=event_request.event_name,
             description=event_request.message,
@@ -23,7 +35,6 @@ def create_event_from_request(modeladmin, request, queryset):
             location=event_request.location,
             capacity=event_request.expected_guests,
         )
-
         created_count += 1
 
     modeladmin.message_user(
@@ -34,7 +45,6 @@ def create_event_from_request(modeladmin, request, queryset):
 
 @admin.register(EventRequest)
 class EventRequestAdmin(admin.ModelAdmin):
-
     list_display = (
         'event_name',
         'name',
